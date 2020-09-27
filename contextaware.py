ab4dfaf17fb704b627766f4406341304c81c0607
Nick Brown <bicknrown@gmail.com>
2020-09-26 23:18:43 -0400

    fixed formatting with output of the contextaware function

--- contextaware.py
+++ contextaware.py
@@ -3,4 +3,4 @@ import subprocess
 def contextaware(message):
     process = subprocess.Popen('cd /home/jimmysmom/JimmysMomBot/gpt2old/src && /home/jimmysmom/JimmysMomBot/venv/bin/python interactive_conditional_samples_context.py --top_k 40 --model_name thegoodone --nsamples 1 --length 100 --wadtext "' + " ".join(message.split().pop(0)) + '"' , stdout=subprocess.PIPE, shell=True)
     processout = process.communicate()[0].strip()
-    return (processout.decode("utf-8")[90:])
+    return (" ".join(message.split().pop(0)) + processout.decode("utf-8")[90:80])
