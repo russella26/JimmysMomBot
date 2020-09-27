86c4db0e089fd8d5a86c67d5ebc47a55d614435a
Nick Brown <bicknrown@gmail.com>
2020-09-26 23:35:15 -0400

    added a space for output formatting in contextaware

--- contextaware.py
+++ contextaware.py
@@ -3,4 +3,4 @@ import subprocess
 def contextaware(message):
     process = subprocess.Popen('cd /home/jimmysmom/JimmysMomBot/gpt2old/src && /home/jimmysmom/JimmysMomBot/venv/bin/python interactive_conditional_samples_context.py --top_k 40 --model_name thegoodone --nsamples 1 --length 100 --wadtext "' + message[5:]  + '"' , stdout=subprocess.PIPE, shell=True)
     processout = process.communicate()[0].strip()
-    return (message[5:] + processout.decode("utf-8"))
+    return (message[5:] + " " + processout.decode("utf-8"))
