29b9c930cc9cec7811c6a767724a844f7be0dd51
Nick Brown <bicknrown@gmail.com>
2020-09-26 21:42:19 -0400

    shellmode does not use a list, adam's bad

--- aitext.py
+++ aitext.py
@@ -1,5 +1,5 @@
 import subprocess
 
 def aitext(message):
-    process = subprocess.run(['cd','/home/jimmysmom/JimmysMomBot/venv/bin','&&','source','activate','&&','cd','/home/jimmysmom/JimmysMomBot/gpt2old/src','&&','python','generate_unconditional_samples.py','--top_k','40','--model_name','tempmod'], stdout=subprocess.PIPE, shell=True)
+    process = subprocess.run("cd /home/jimmysmom/JimmysMomBot/venv/bin && source activate && cd /home/jimmysmom/JimmysMomBot/gpt2old/src && python generate_unconditional_samples.py --top_k 40 --model_name tempmod", stdout=subprocess.PIPE, shell=True)
     return (process.stdout.decode('utf-8'))
