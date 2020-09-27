6634aaa4a70e0607444f7a737d66f75b00aef988
Nick Brown <bicknrown@gmail.com>
2020-09-26 21:52:46 -0400

    changed the command in aitext because venv does not seem to work

--- aitext.py
+++ aitext.py
@@ -1,6 +1,6 @@
 import subprocess
 
 def aitext(message):
-    process = subprocess.Popen("cd /home/jimmysmom/JimmysMomBot/venv/bin && source activate && cd /home/jimmysmom/JimmysMomBot/gpt2old/src && python generate_unconditional_samples.py --top_k 40 --model_name tempmod", stdout=subprocess.PIPE, shell=True)
+    process = subprocess.Popen("cd /home/jimmysmom/JimmysMomBot/gpt2old/src && /home/jimmysmom/JimmysMomBot/venv/bin/python generate_unconditional_samples.py --top_k 40 --model_name tempmod", stdout=subprocess.PIPE, shell=True)
     processout = process.communicate()[0].strip()
-    return (processout)
+    return (processout.decode("utf-8"))
