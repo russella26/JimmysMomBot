72da4e34d1d7a73270586ee4f07d219b952f9aae
Nick Brown <bicknrown@gmail.com>
2020-09-26 21:28:08 -0400

    used subprocess to actually run a command to get a random output from the model

--- aitext.py
+++ aitext.py
@@ -1,6 +1,5 @@
 import subprocess
-import re
 
 def aitext(message):
-    bigstring = subprocess.check_output('cd /home/jimmysmom/JimmysMomBot/venv/bin && source activate && cd /home/jimmysmom/JimmysMomBot/gpt2old/src && python generate_unconditional_samples.py --top_k 40 --model_name tempmod')
-    return (bigstring)
+    process = subprocess.run(['cd','/home/jimmysmom/JimmysMomBot/venv/bin','&&','source','activate','&&','cd','/home/jimmysmom/JimmysMomBot/gpt2old/src','&&','python','generate_unconditional_samples.py','--top_k','40','--model_name','tempmod'], stdout=subprocess.PIPE)
+    return (process.stdout.decode('utf-8'))
