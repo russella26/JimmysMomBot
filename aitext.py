58bafed35dadf43a5e3fd604fafbadcb5efa35fc
Nick Brown <bicknrown@gmail.com>
2020-09-26 22:47:22 -0400

    updated the model

--- aitext.py
+++ aitext.py
@@ -1,6 +1,6 @@
 import subprocess
 
 def aitext(message):
-    process = subprocess.Popen("cd /home/jimmysmom/JimmysMomBot/gpt2old/src && /home/jimmysmom/JimmysMomBot/venv/bin/python generate_unconditional_samples.py --top_k 40 --model_name tempmod --nsamples 1 --length 50", stdout=subprocess.PIPE, shell=True)
+    process = subprocess.Popen("cd /home/jimmysmom/JimmysMomBot/gpt2old/src && /home/jimmysmom/JimmysMomBot/venv/bin/python generate_unconditional_samples.py --top_k 40 --model_name thegoodone --nsamples 1 --length 100", stdout=subprocess.PIPE, shell=True)
     processout = process.communicate()[0].strip()
     return (processout.decode("utf-8")[90:])
