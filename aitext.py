51b1ab8a4c934d66324da009423c242146d7b21e
Nick Brown <bicknrown@gmail.com>
2020-09-26 21:14:21 -0400

    changed call for rand aitext, it is a lot more simple now

--- aitext.py
+++ aitext.py
@@ -1,15 +1,6 @@
-import os
+import subprocess
 import re
 
 def aitext(message):
-    os.system('cd /home/jimmysmom/JimmysMomBot/venv/bin && source activate && cd /home/jimmysmom/JimmysMomBot/gpt2old && python generate_unconditional_samples.py --top_k 40 --model_name tempmod >> ../temp.txt')
-    f = open("temp.txt")
-    first = False
-    bigstring = ""
-    for line in f:
-        if first:
-             bigstring += line
-        first = True
-    f.close()
-    os.system("rm temp.txt")
+    bigstring = subprocess.check_output('cd /home/jimmysmom/JimmysMomBot/venv/bin && source activate && cd /home/jimmysmom/JimmysMomBot/gpt2old/src && python generate_unconditional_samples.py --top_k 40 --model_name tempmod')
     return (bigstring)
